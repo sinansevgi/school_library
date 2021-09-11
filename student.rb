bd88f53af6ace5a0e6bbf854ab647ef4562f621b
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-12 02:21:02 +0300

    Fix linter errors

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative 'person.rb'
 class Student < Person
   def initialize(name = 'Unknown', parent_permission = true, age, classroom)
-    super(name,parent_permission,age)
+    super(name, parent_permission, age)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person.rb'
 class Teacher < Person
   def initialize(name = 'Unknown', parent_permission = true, age, specialization)
-    super(name,parent_permission,age)
+    super(name, parent_permission, age)
     @specialization = specialization
   end
 
