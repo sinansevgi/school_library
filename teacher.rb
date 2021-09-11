097464c0235cf74b8d9b5dea95f638e5eb971314
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-12 02:13:24 +0300

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require_relative 'person.rb'
+class Teacher < Person
+  def initialize(name = 'Unknown', parent_permission = true, age, specialization)
+    super(name,parent_permission,age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
