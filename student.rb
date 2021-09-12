cdb13be1343af05c1d5cf9905c7eb3f9430b6635
Sinan Sevgi <sinansevgi@users.noreply.github.com>
2021-09-12 03:10:04 +0300

    Add classroom class

--- student.rb
+++ student.rb
@@ -1,8 +1,11 @@
 require_relative 'person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, name: 'Unknown')
     super(name)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
