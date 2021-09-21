7206e9e220503b0da38689e0e0b3bfa90dfd63bf
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-09-21 13:20:39 +0200

    Add create_book and list_books

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-
 require_relative 'teacher'
 require_relative 'student'
 require_relative 'book'
@@ -15,6 +14,9 @@ def list_people
 end
 
 def list_books
+  Book.all.each_with_index do |book, index|
+    puts "#{index}) Title:#{book.title}, Author:#{book.author}"
+  end
 end
 
 def list_rentals
@@ -48,19 +50,55 @@ def create_person
       specialization,
     )
   end
-  p list_people
+
   puts 'Person created succesfully'
 end
 
 def create_book
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  author = gets.chomp
+  Book.new(title, author)
 
+  puts 'Person created succesfully'
 end
 
 def create_rental
 end
 
-
-def main
+def list_person_rentals
+  # print 'ID: '
+  # id = gets.chomp.to_i
+  # person = Person.all.filter{|person| person.id == id}
+  # p person
 end
 
-create_person
+def main
+  puts '1 - List all people'
+  puts '2 - List all books'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+  case gets.chomp.to_i
+  when 1
+    list_people
+  when 2
+    list_books
+  when 3
+    create_person
+  when 4
+    create_book
+  when 5
+    create_rental
+  when 6
+    list_person_rentals
+  else
+    return
+  end
+  main
+end
+
+main
